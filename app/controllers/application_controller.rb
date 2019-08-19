642732de8782ab17716d70f839f8954375336a39
Ignacio <imoon521@gmail.com>
2019-08-18 19:06:08 -0600

    Wallets crud

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::API 
+   before_action :cors_set_access_control_headers
+
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, PATCH, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = '*'
+  end
+  
   include Response
   include ExceptionHandler
   include DeviseTokenAuth::Concerns::SetUserByToken
