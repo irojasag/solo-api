a64595fb7bb8de09fbd105f6a8266f617f80a06e
Ignacio <imoon521@gmail.com>
2019-08-24 10:43:08 -0600

    Fix precision issue

--- db/migrate/20190816211619_create_wallets.rb
+++ db/migrate/20190816211619_create_wallets.rb
@@ -2,7 +2,7 @@ class CreateWallets < ActiveRecord::Migration[5.2]
   def change
     create_table :wallets do |t|
       t.string :name, :limit => 50
-      t.decimal :initial_balance, precision: 2, scale: 2
+      t.decimal :initial_balance, precision: 15, scale: 5
       t.references :user, index: true, foreign_key: true
       t.timestamps
     end
